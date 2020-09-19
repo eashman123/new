77dac241a4de509166e8cfb1d23c9796399b4bd6
Eashaan Katiyar <eashaank@gmail.com>
2020-09-19 01:20:20 -0700

    Updated for 2021 Season

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 //jquery
+
+$("body").toggleClass("dark");
+$(".name").toggleClass("dark");
+$(".foot").toggleClass("dark");
+$(".icon").toggleClass("dark");
+$(".nav-link").toggleClass("dark");
+$(".navbar").toggleClass("dark");
+$(".to-top").toggleClass("dark");
+$(".card-title").toggleClass("dark");
+$(".resume:after").toggleClass("dark");
+$(".card").toggleClass("dark");
+$(".major").toggleClass("dark");
+$(".list-group-mine .list-group-item").toggleClass("dark");
+$(".list-group-mine .list-group-item:hover").toggleClass("dark");
+$(".list-group-mine .list-group-item:active").toggleClass("dark");
+$(".list-bar").toggleClass("please");
+$(".just-head-two").toggleClass("please");
+$(".rrr").toggleClass("please");
+$(".nav-link-two").toggleClass("please");
+$(".navbrdr-two").toggleClass("please");
+$(".night-day").toggleClass("fa-sun-o fa-moon-o");
+$(".lock-two").toggleClass("please");
+$(".date").toggleClass("dark");
+$(".p-foot").toggleClass("dark");
+$(".cs61b").toggleClass("dark");
+$(".card-footer").toggleClass("dark");
+$(".resume-two-bar").toggleClass("please");
+
 $(document).ready(function() {
   $(".night-day").click(function() {
     $("body").toggleClass("dark");
